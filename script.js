81fcac57a05f791889f523e169d453e76b506f80
NeoSeuc <vitaliy199718@gmail.com>
2019-06-19 20:49:32 +0300

    promise refactoring

--- script.js
+++ script.js
@@ -7,44 +7,54 @@ function apiSearch(e) {
     const searchText = searchInput.value,
         url = "https://api.themoviedb.org/3/search/movie?api_key=30bcec8f5dda885072b3eeb762fe32c3&language=ru&query=" + searchText + "&page=1&include_adult=false";
     if (searchText)
-        requestApi(url)
+        requestApi(url).then((res) => {
+            const result = JSON.parse(res);
+            let markup = "";
+            for (let i = 0; i < result.total_results; i++) {
+                let obj = result.results[i];
+                if (!obj)
+                    continue;
+                let name = obj.title || obj.name;
+
+                markup += getMovieMarkup(name, obj.overview, obj.poster_path);
+            }
+            movies.innerHTML = markup;
+        }).catch((res)=>{
+
+        });
 }
 
 searchForm.addEventListener('submit', apiSearch);
 
 function requestApi(url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("get", url);
-    xhr.send();
-    xhr.addEventListener('readystatechange', ()=>{
-       if (xhr.readyState!==4) {
-           movies.innerHTML = "Загрузка";
-           return;
-       }
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            if (xhr.status !== 200) {
+                reject({
+                    status: xhr.status,
+                });
+                return;
+            }
 
-       if (xhr.status!==200) {
-           movies.innerHTML = "что-то пошло не так";
-           return;
-       }
-
-        const result = JSON.parse(xhr.response);
-        let markup = "";
-        for (let i = 0; i < result.total_results; i++) {
-            let obj = result.results[i];
-            if (!obj)
-                continue;
-            let name = obj.title || obj.name;
-
-            markup+=getMovieMarkup(name, obj.overview, obj.poster_path);
-        }
-        movies.innerHTML = markup;
+            resolve(xhr.response);
+        });
+        xhr.addEventListener('error', () => {
+            reject({
+                status: xhr.status,
+            });
+        });
     });
+
+
 }
 
 function getMovieMarkup(name, description, img) {
     name = name.substr(0, 30);
-    description = description.substr(0, 60)+"...";
-    img = "https://image.tmdb.org/t/p/w500"+img;
+    description = description.substr(0, 60) + "...";
+    img = "https://image.tmdb.org/t/p/w500" + img;
     return '<div class="col">\n' +
         '           <div class="card mb-2" style="width: 18rem;">\n' +
         '               <img src="' + img + '" class="card-img-top" alt="...">\n' +
